9f62a990b522c498da26883cbe3f4aaa2d03aff8
Tanay5599 <tbhandare800@gamai.com>
2023-12-20 10:04:07 +0530

    | | Index Page | |

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const express = require("express");
 const expressApp = express();
 const cors = require("cors");
 const path = require("path");
-const checked = require("./src/db/db_config");
+// const checked = require("./src/db/db_config");
 // const checkPermissions = require("./src/auth/permissionMiddleware");
 const sqlite3 = require("sqlite3").verbose();
 const fs = require("fs");
 const os = require("os");
 // const authenticateToken = require("./src/auth/authMiddleware");
-const url_helper = require("./url_helper");
+// const url_helper = require("./url_helper");
 const username = os.userInfo().username;
 
 const imagesPath = `C:\\Users\\${username}\\AppData\\Roaming\\RkSalesCrm\\all_images`;
@@ -17,62 +17,63 @@ expressApp.use(express.static(imagesPath));
 expressApp.use(express.json());
 expressApp.use(cors());
 // create data
-checked
-  .sync({ force: false }) // if false then not drop created table else it will drop tables and every time gives new tables
-  .then(() => {
-    require("./db/create_data");
-    console.log("Database created!");
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+// checked
+//   .sync({ force: false }) // if false then not drop created table else it will drop tables and every time gives new tables
+//   .then(() => {
+//     require("./db/create_data");
+//     console.log("Database created!");
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
 
 // city routes
 const city = require("./src/city/city_route");
-expressApp.use("/api", city);
+expressApp.use("/back-end", city);
 
-// permission
+// // permission
 
-// source routes
+// // source routes
 const source = require("./src/source/source_route");
-expressApp.use("/api", source);
+expressApp.use("/back-end", source);
 
-// source routes
+// // source routes
 const branch = require("./src/branch/branch_route");
-expressApp.use("/api", branch);
+expressApp.use("/back-end", branch);
 
 // category routes
 const category = require("./src/category/category_routes");
-expressApp.use("/api", category);
+expressApp.use("/back-end", category);
 
 // product routes
 const product = require("./src/product/product_routes");
-expressApp.use("/api", product);
+expressApp.use("/back-end", product);
 
 // Reference route
 const Reference = require("./src/reference/reference_routes");
-expressApp.use("/api", Reference);
+expressApp.use("/back-end", Reference);
 
 // lead status routes
 const leadStatus = require("./src/lead_status/lead_route");
-expressApp.use("/api", leadStatus);
+expressApp.use("/back-end", leadStatus);
 
 // roles routes
 const roles = require("./src/roles/roles_routes");
-expressApp.use("/api", roles);
+expressApp.use("/back-end", roles);
 
 // point routes
-const point = require("./src/point/point_routes");
-expressApp.use("/api", point);
+// const point = require("./src/point/point_routes");
+// expressApp.use("/back-end", point);
 
 // customer route
 const Customer = require("./src/customer/customer_routes");
-expressApp.use("/api", Customer);
+expressApp.use("/back-end", Customer);
 
 // employee route
-const Employee = require("./src/employee/employee_route");
-expressApp.use("/api", Employee);
+// const Employee = require("./src/employee/employee_route");
+// expressApp.use("/back-end", Employee);
 // ################################ END #####################################################
-expressApp.listen(8880, () => {
-  console.log(`Server is running on http://localhost:${8880}`);
+const port = process.env.PORT || 8880;
+expressApp.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
