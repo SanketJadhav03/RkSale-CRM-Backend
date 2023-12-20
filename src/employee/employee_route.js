e8b382932c5f50d25d3962d2a1733530ee936825
Tanay5599 <tbhandare800@gamai.com>
2023-12-20 18:31:02 +0530

    | | Employee Delete | |

--- src/employee/employee_route.js
+++ src/employee/employee_route.js
@@ -1,43 +1,9 @@
 const router = require("express").Router();
 const controller = require("./employee_controller");
-// const multer = require("multer");
 
-// const storage = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     cb(null, `public/allimages`);
-//   },
-//   filename: function (req, file, cb) {
-//     cb(null, Date.now() + "-" + file.originalname);
-//   },
-// });
-
-// const upload = multer({ storage: storage });
-
-// GET all employees
-// router.get("/employee/list", controller.index);
-
-// POST create a new employee with multiple image upload
-router.post(
-  "/employee/store",
-  // upload.fields([
-  //   { name: "employee_aadhar", maxCount: 1 },
-  //   { name: "employee_pan", maxCount: 1 },
-  //   { name: "employee_profile", maxCount: 1 },
-  //   { name: "employee_qr_code", maxCount: 1 },
-  // ]),
-  controller.store
-);
-
-// // GET a specific employee by ID
-// router.get("/employee/show/:id", controller.show);
-
-// // PUT update a specific employee by ID with multiple image upload
-// router.put(
- 
-//   controller.updated
-// );
-
-// // DELETE delete a specific employee by ID
-// router.delete("/employee/delete/:id", controller.deleted);
+router.post("/employee/store", controller.store);
+router.get("/employee/list",controller.index)
+router.get('/employee/show/:id',controller.show)
+router.delete('/employee/delete/:id',controller.deleted)
 
 module.exports = router;
