65d382c4c60bddc16a44a95e88f6d2d883f40aec
adeshtilekar <tilekaradesh@gmail.com>
2023-12-23 15:11:22 +0530

    Lead Status

--- src/lead_status/lead_model.js
+++ src/lead_status/lead_model.js
@@ -1,18 +1,18 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../db/db_config'); // Your Sequelize connection
 
-const LeadStatus = sequelize.define('tbl_lead_statuses', {
-  lead_id: {
+const LeadStatus = sequelize.define('tbl_lead_status', {
+  lead_status_id: {
     type: DataTypes.INTEGER,
     allowNull: false,
     primaryKey: true,
     autoIncrement: true,
   },
-  lead_name: {
+  lead_status_name: {
     type: DataTypes.STRING,
     allowNull: false,
   },
-  lead_status: {
+  lead_status_status: {
     type: DataTypes.INTEGER,
     defaultValue: 1,
     allowNull: false,
