58c93a151c69f74307f46bb52bc14bdce859fd89
adeshtilekar <tilekaradesh@gmail.com>
2024-01-03 17:03:03 +0530

    ..//

--- src/payment_mode/payment_controller.js
+++ src/payment_mode/payment_controller.js
@@ -1,81 +1,81 @@
 const Payment_Mode = require("./payment_mode_model");
 
 
-const store = async(req,res)=>{
-try {
-    const {payment_mode_name} = req.body;
+const store = async (req, res) => {
+    try {
+        const { payment_mode_name } = req.body;
 
-    const new_Payment_Mode = await Payment_Mode.create({
-        payment_mode_name
-    })
-    
-    if (new_Payment_Mode){
-        res.json({message:"Payment Mode Created SuccessFully"})
-    } 
-} catch (error) {
-    console.log(error);
-    res.json({error:"Failed To Store Payment Mode "})
+        const new_Payment_Mode = await Payment_Mode.create({
+            payment_mode_name
+        })
+
+        if (new_Payment_Mode) {
+            res.json({ message: "Payment Mode Created SuccessFully", status: 1 })
+        }
+    } catch (error) {
+        console.log(error);
+        res.json({ error: "Failed To Store Payment Mode " })
+    }
+
 }
 
-}
-
-const index = async(req,res) =>{
+const index = async (req, res) => {
     try {
         const payment_modes = await Payment_Mode.findAll();
         res.json(payment_modes)
     } catch (error) {
         console.log(error);
-        res.json({error:"Failed To Show Payment Mode "})
+        res.json({ error: "Failed To Show Payment Mode " })
     }
 }
 
-const update = async(req,res)=>{
-try {
-    const {id,payment_mode_name}= req.body;
-    const existingpayment_mode = await Payment_Mode.findByPk(id);
-    if (existingpayment_mode){
-        const updated_payment_mode = await existingpayment_mode.update({
-            payment_mode_name:payment_mode_name
-        })
-        if(updated_payment_mode){
-            res.json({message:"Updated SuccessFully"})
-        }
-    }else {
-        res.json({message:"Payment Mode Not Found"})
-    }
-} catch (error) {
-    console.log("Error For Updating The Updation Of Payment");
-}
-}
-
-const deleted = async(req,res)=>{
+const update = async (req, res) => {
     try {
-        const {id} = req.params;
-        const existin_payment_mode = await Payment_Mode.findByPk(id);
-        if (existin_payment_mode){
-            const status = await existin_payment_mode.destroy();
-            if (status){
-                res.json({message:"Deleted SuccessFully"})
+        const { payment_mode_id, payment_mode_name } = req.body;
+        const existingpayment_mode = await Payment_Mode.findByPk(payment_mode_id);
+        if (existingpayment_mode) {
+            const updated_payment_mode = await existingpayment_mode.update({
+                payment_mode_name: payment_mode_name
+            })
+            if (updated_payment_mode) {
+                res.json({ message: "Updated SuccessFully", status: 1 })
             }
-        }else {
-            res.json({message:"Payment Mode Not Found"})
+        } else {
+            res.json({ message: "Payment Mode Not Found" })
         }
     } catch (error) {
-        
+        console.log("Error For Updating The Updation Of Payment");
+    }
+}
+
+const deleted = async (req, res) => {
+    try {
+        const { payment_mode_id } = req.params;
+        const existin_payment_mode = await Payment_Mode.findByPk(payment_mode_id);
+        if (existin_payment_mode) {
+            const status = await existin_payment_mode.destroy();
+            if (status) {
+                res.json({ message: "Deleted SuccessFully" })
+            }
+        } else {
+            res.json({ message: "Payment Mode Not Found" })
+        }
+    } catch (error) {
+
     }
 
 }
-const show = async(req,res)=>{
+const show = async (req, res) => {
     try {
-        const {id} = req.params;
-        const payement_mode = await Payment_Mode.findByPk(id);
-        if (payement_mode){
-           res.json(payement_mode)
-        }else {
-            res.json({message:"Payment Mode Not Found"})
+        const { payment_mode_id } = req.params;
+        const payement_mode = await Payment_Mode.findByPk(payment_mode_id);
+        if (payement_mode) {
+            res.json(payement_mode)
+        } else {
+            res.json({ message: "Payment Mode Not Found" })
         }
     } catch (error) {
-        
+
     }
 
 }
--- src/payment_mode/payment_mode_routes.js
+++ src/payment_mode/payment_mode_routes.js
@@ -3,10 +3,10 @@ const router = express.Router();
 
 const controller = require('./payment_controller')
 
-router.post('/payment_mode/store',controller.store)
-router.post('/payment_mode/list',controller.index)
-router.post('/payment_mode/show/:id',controller.show)
-router.post('/payment_mode/update',controller.update)
-router.post('/payment_mode/delete/:id',controller.deleted)
+router.post('/payment_mode/store', controller.store)
+router.get('/payment_mode/list', controller.index)
+router.get('/payment_mode/show/:id', controller.show)
+router.put('/payment_mode/update', controller.update)
+router.delete('/payment_mode/delete/:id', controller.deleted)
 
 module.exports = router;
