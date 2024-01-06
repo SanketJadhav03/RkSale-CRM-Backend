e1bd9ce8fd9916bbb3f920465846b9b67df6c473
SanketJadhav03 <jadhavsanket03@gmail.com>
2024-01-06 16:07:37 +0530

    :::: Follow Up Controller Updated ::::

--- src/follow_up/follow_up_controller.js
+++ src/follow_up/follow_up_controller.js
@@ -1,8 +1,44 @@
+const { QueryTypes } = require("sequelize");
+const sequelize = require("../db/db_config");
 const FollowUp = require("./follow_up_model");
 const index = async (req, res) => {
   try {
-    await FollowUp.findAll();
-  } catch (error) {}
+    const { leadOrTask, follow_up_send_by } = req.body;
+    let query = ``;
+    if (leadOrTask == 0) {
+      query += `
+      SELECT tbl_follow_ups.createdAt as created_date,tbl_leads.*,tbl_lead_statuses.*,tbl_sources.*,tbl_follow_ups.*,tbl_customers.*,tbl_cities.*,tbl_customer_groups.*,tbl_references.*,tbl_products.*
+    FROM tbl_follow_ups
+      INNER JOIN tbl_leads ON tbl_follow_ups.follow_up_lead_id = tbl_leads.lead_id 
+      INNER JOIN tbl_customers ON tbl_leads.customer = tbl_customers.customer_id
+      INNER JOIN tbl_cities ON tbl_customers.customer_city = tbl_cities.city_id
+      INNER JOIN tbl_customer_groups ON tbl_customers.customer_group = tbl_customer_groups.customer_group_id
+      INNER JOIN tbl_products ON tbl_leads.product = tbl_products.product_id
+      INNER JOIN tbl_references ON tbl_leads.ref_by = tbl_references.reference_id
+      INNER JOIN tbl_sources ON tbl_leads.source = tbl_sources.source_id
+      INNER JOIN tbl_lead_statuses ON tbl_leads.status = tbl_lead_statuses.lead_status_id
+      WHERE tbl_follow_ups.follow_up_send_by= ${follow_up_send_by};`;
+    } else {
+      query += `
+      SELECT tbl_follow_ups.createdAt as created_date,tbl_tasks.*,tbl_follow_ups.*,tbl_customers.*,tbl_products.*,tbl_references.*,tbl_lead_statuses.*,tbl_sources.*
+    FROM tbl_follow_ups
+      INNER JOIN tbl_tasks ON tbl_follow_ups.follow_up_task_id = tbl_tasks.task_id 
+      INNER JOIN tbl_customers ON tbl_tasks.customer = tbl_customers.customer_id
+      INNER JOIN tbl_products ON tbl_tasks.product = tbl_products.product_id
+      INNER JOIN tbl_references ON tbl_tasks.ref_by = tbl_references.reference_id
+      INNER JOIN tbl_lead_statuses ON tbl_tasks.status = tbl_lead_statuses.lead_status_id
+      INNER JOIN tbl_sources ON tbl_tasks.source = tbl_sources.source_id
+      WHERE tbl_follow_ups.follow_up_send_by= ${follow_up_send_by};`;
+    }
+    const data = await sequelize.query(query, {
+      type: QueryTypes.SELECT,
+      model: FollowUp, // Specify the model for Sequelize to map the result to
+    });
+    res.json(data);
+  } catch (error) {
+    console.log(error);
+    res.json({ error: "Failed To Get follow up data" });
+  }
 };
 const addFollowUp = async (req, res) => {
   try {
