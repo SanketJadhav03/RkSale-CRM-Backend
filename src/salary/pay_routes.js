a03dd4de44ef75f1f64a56af1908dccab36e939f
adeshtilekar <tilekaradesh@gmail.com>
2024-05-20 15:09:14 +0530

    paycontroller done

--- src/salary/pay_routes.js
+++ src/salary/pay_routes.js
@@ -4,6 +4,6 @@ const PayController = require('./pay_controller')
 
 router.get('/pay/calculation/:employee_id', PayController.calculationSalary);
 router.post('/pay/store', PayController.store);
-
+router.get('/slip/view/:id', PayController.view)
 
 module.exports = router;
