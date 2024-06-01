449c990435e2cbbf494c7e9c8ef399423e98024a
adeshtilekar <tilekaradesh@gmail.com>
2024-06-01 12:09:51 +0530

    followup route create

--- src/follow_up/follow_up_route.js
+++ src/follow_up/follow_up_route.js
@@ -4,6 +4,7 @@ const controller = require("./follow_up_controller");
 
 router.post("/follow_up/lead", controller.addFollowUp);
 
-router.post("/follow_up/list",controller.index);
+router.post("/follow_up/list", controller.index);
+router.post("/follow_up", controller.followup);
 
 module.exports = router;
